ca2f4ef5438e866d5a52258649392694c907b3d4
Alejandro <alejandrogomcap@campus.monlau.com>
2025-02-09 21:48:31 +0100

    holl

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -13,11 +13,17 @@ class TestController extends Controller
         try {
             echo 'hola';
             Log::info('TestController@index: Start');
+            
+            // Verificar la conexión a la base de datos
+            if (!\DB::connection()->getDatabaseName()) {
+                throw new \Exception('No se puede conectar a la base de datos');
+            }
+    
             $users = User::all();
             Log::info('TestController@index: Retrieved users', ['count' => $users->count()]);
             return response()->json($users);
         } catch (\Exception $e) {
-            Log::error('TestController@index: Error', ['message' => $e->getMessage()]);
+            Log::error('TestController@index: Error', ['message' => $e->getMessage(), 'trace' => $e->getTraceAsString()]);
             return response()->json(['error' => 'An error occurred'], 500);
         }
     }

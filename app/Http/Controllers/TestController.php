1ec7d1bc6d98caea4002d891bbce11f212d8271f
Alejandro <alejandrogomcap@campus.monlau.com>
2025-02-09 21:45:09 +0100

    commi

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,8 +10,15 @@ class TestController extends Controller
 {
     public function index()
     {
-        
+        try {
             echo 'hola';
-      
+            Log::info('TestController@index: Start');
+            $users = User::all();
+            Log::info('TestController@index: Retrieved users', ['count' => $users->count()]);
+            return response()->json($users);
+        } catch (\Exception $e) {
+            Log::error('TestController@index: Error', ['message' => $e->getMessage()]);
+            return response()->json(['error' => 'An error occurred'], 500);
+        }
     }
 }
